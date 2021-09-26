0d2486d4a8fbcd67eb6d4b0614d63a08d222077a
sherifcodeur <sherifcodeur@gmail.com>
2021-09-26 22:23:06 +0100

    try to extract data from schema to generate crud

--- models/Problem.js
+++ models/Problem.js
@@ -26,6 +26,28 @@
     const Problem = mongoose.model('problem',problemSchema);
 
 
+    // // we want to loop through the fields and send an object that will help us generate the crud   
+    // // extracts the paths (c a dire les fields)
+    // const testo =  problemSchema.paths ;  
+
+    
+    // for(key in testo){
+    //     // this gives us the field name
+    //     console.log(key)
+
+    //     // this gives us the type of field
+    //     console.log(testo[key].instance)
+    // }
+
+
+
+    
+
+    
+
+   
+
+
 
     //exporting the model Problem
     module.exports =  Problem ;
