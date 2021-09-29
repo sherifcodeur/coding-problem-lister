623020afefbcd279903b145d4d4e8235a76dc02a
sherifcodeur <sherifcodeur@gmail.com>
2021-09-29 16:55:37 +0100

    integrate style in head and js in footer

--- models/Problem.js
+++ models/Problem.js
@@ -40,6 +40,12 @@
         email:{
             type:String,
             
+        },
+        tag:{
+            type:Array,
+        },
+        due:{
+            type:Date,
         }
         
     
